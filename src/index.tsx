0893853499554a57ed918de679c03128e7665ab0
Xiaolin Liu <lixiaoli@zeliu08.redmond.corp.microsoft.com>
2021-08-15 23:40:05 +0800

    chore: add demo stories

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import * as PIXI from "pixi.js";
+import React from "react";
+
+export const Example: React.FunctionComponent = () => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+  const bunny = PIXI.Sprite.from("src/icon.png");
+  bunny.anchor.set(0.5);
+
+  // move the sprite to the center of the screen
+  bunny.x = app.screen.width / 2;
+  bunny.y = app.screen.height / 2;
+
+  app.stage.addChild(bunny);
+
+  // Listen for animate update
+  app.ticker.add((delta) => {
+    // just for fun, let's rotate mr rabbit a little
+    // delta is 1 if running at 100% performance
+    // creates frame-independent transformation
+    bunny.rotation += 0.1 * delta;
+  });
+  React.useEffect(() => {
+    ref.current?.appendChild(app.view);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <div ref={ref} />;
+};
---
+++ stories/Demos.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Example } from "../src";
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: "Demos/flow",
+};
+
+export const Basic = () => {
+  return <Example />;
+};
