6e649047cf829e751e1e978178e85244db206824
Xiaolin Liu <--local>
2021-08-26 16:59:23 +0800

    misc: add graph interface

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export interface INode<T = unknown, P = unknown> {
+  readonly id: string;
+  readonly x: number;
+  readonly y: number;
+  readonly name?: string;
+  readonly width?: number;
+  readonly height?: number;
+  readonly ports?: ReadonlyArray<IPort<P>>;
+  readonly data?: Readonly<T>;
+}
+
+export interface IEdge<T = unknown> {
+  readonly id: string;
+  readonly source: string;
+  readonly target: string;
+  readonly sourcePort: string;
+  readonly targetPort: string;
+  readonly data?: Readonly<T>;
+}
+
+export interface IPort<T = unknown> {
+  readonly id: string;
+  readonly x: number;
+  readonly y: number;
+  readonly name?: string;
+  readonly data?: Readonly<T>;
+}
+
+export interface IGraph<
+  NodeData = unknown,
+  EdgeData = unknown,
+  PortData = unknown
+> {
+  readonly nodes: ReadonlyArray<INode<NodeData, PortData>>;
+  readonly edges: ReadonlyArray<IEdge<EdgeData>>;
+}
