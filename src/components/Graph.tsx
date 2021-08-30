6e870317a15cfee57f08c5c2fcdcdf9258470b03
Xiaolin Liu <iuiaoin@gmail.com>
2021-08-30 14:13:07 +0800

    misc: add sample data

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -4,13 +4,13 @@ import { IGraphProps } from "../types";
 
 export const Graph: React.FunctionComponent<IGraphProps> = (props) => {
   const { nodes, backgroundColor } = props;
-  const app = new Application({ backgroundColor });
+  const app = new Application({ backgroundColor, antialias: true });
   const ref = React.useRef<HTMLDivElement>(null);
 
   nodes.forEach((node) => {
     const rect = new Graphics();
     rect.lineStyle(1, 0x8a8886, 1);
-    rect.drawRoundedRect(0.5, 0.5, 272, 54, 10);
+    rect.drawRoundedRect(0.5, 0.5, 262, 52, 4);
     rect.endFill();
     app.stage.addChild(rect);
   });
